32285fe0b31d299238d47fe804c4042def3ac857
yanis21 <khalfaouianis3100@gmail.com>
2022-10-12 22:16:04 +0100

    update

--- models/Author.js
+++ models/Author.js
@@ -1,35 +1,18 @@
 const { string } = require("joi");
 const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
 
-const AuthorSchema = new mongoose.Schema ({
 
-    firstname :
-    {
-        type: string,
-        required: true,
-        trim : true ,
-        minlength : 3,
-        maxlength : 200,
+const AuthorSchema = new Schema ({
 
+    
+    firstname: {
+        type: String
     },
-    lastname :
-    {
-        type: string,
-        required: true,
-        trim : true ,
-        minlength : 3,
-        maxlength : 200,
-
+    lastName: {
+        type: String,
     },
-    nationality:
-    {
-        type: string,
-        required: true,
-        trim : true ,
-        minlength : 2,
-        maxlength : 100,
-
-    },
+   
 },{
 timestamps:true
 });
